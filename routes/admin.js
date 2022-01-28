608cebed994aee499e568ef10f2e80166a6f5627
Hamza ejjraifi <76843028+ejjraifihamza@users.noreply.github.com>
2022-01-28 10:14:15 +0000

    🚀 Update Customer Functionality

--- routes/admin.js
+++ routes/admin.js
@@ -1,8 +1,7 @@
 const router = require("express").Router();
 const mongodb = require("mongodb");
-const Client = require('../model/client');
 
-const ObjectId = mongodb.ObjectId
+const ObjectId = mongodb.ObjectId;
 
 const Hotel = require("../model/hotel");
 const Customer = require("../model/customer");
@@ -35,71 +34,64 @@ router.delete("/hotel/:id/delete", (req, res) => {
 
 // CRUD for customers registered on the site.
 router.get("/customers", async (req, res) => {
-   const customers = await Client.find()
-   console.log(customers);
-   res.send(customers)
-
+  const customers = await Customer.find();
+  res.send(customers);
 });
 
 router.post("/add-customer", async (req, res) => {
- const {name, email, password} = req.body;
- const client = new Client({
-   name:name,
-   email:email,
-   password:password
- })
- try {
-   await client.save();
-   res.status(200).send("client created suc")
- }
- catch( error){
-   res.status(400).send(error);
- }
+  const { name, email, password } = req.body;
+  const client = new Customer({
+    name: name,
+    email: email,
+    password: password,
+  });
+  try {
+    await client.save();
+    return res.status(200).send("client created suc");
+  } catch (error) {
+    res.status(400).send(error);
+  }
 });
 
 router.get("/customer/:id", async (req, res) => {
-  console.log(req.params.id)
-  const id = req.params.id
-  console.log(id)
-  
-  const client = await Client.findOne({_id: new ObjectId(id)})
-  if(!client) return res.send('not found')
-  res.send(client)
+  const id = req.params.id;
+  const client = await Customer.findOne({ _id: new ObjectId(id) });
+  if (!client) return res.send("not found");
+  res.send(client);
 });
 
 router.patch("/customer/:id/update", async (req, res) => {
-  const id = new ObjectId(req.params.id)
-  const {name, email, password} = req.body
+  const id = new ObjectId(req.params.id);
+  const { name, email, password } = req.body;
   const newCustomer = {
     name: name,
     email: email,
-    password: password
-  }
+    password: password,
+  };
 
-  const client = await Client.findOne({_id: new ObjectId(id)})
-  if(!client) return res.send('Client not found')
-  const updateClient = await Client.updateOne({_id: id}, newCustomer)
-  if(updateClient) res.send('user updated successfully!')
-  res.send('somthing goes wrong')
+  const client = await Customer.findOne({ _id: new ObjectId(id) });
+  if (!client) return res.send("Client not found");
+  const updateClient = await Customer.updateOne({ _id: id }, newCustomer);
+  if (updateClient) return res.send("user updated successfully!");
+  res.send("somthing goes wrong");
 });
 
 router.delete("/customer/:id/delete", async (req, res) => {
-  const id = new ObjectId(req.params.id)
-  const client = await Client.findOne({_id: new ObjectId(id)})
-  if(!client) return res.send('Client not found')
-  const deleteClient = await Client.deleteOne({_id: id})
-  if(updateClient) res.send('user deleted successfully!')
-  res.send('somthing goes wrong')
+  const id = new ObjectId(req.params.id);
+  const client = await Customer.findOne({ _id: new ObjectId(id) });
+  if (!client) return res.send("Client not found");
+  const deleteClient = await Customer.deleteOne({ _id: id });
+  if (deleteClient) return res.send("user deleted successfully!");
+  res.send("somthing goes wrong");
 });
 
 // CRUD for owners registered on the site.
 router.get("/owners", async (req, res) => {
   try {
-    const owner = await Owner.find({})
-      .catch(err => {
-        console.error(err);
-      })
-    res.status(200).send(owner)
+    const owner = await Owner.find({}).catch((err) => {
+      console.error(err);
+    });
+    res.status(200).send(owner);
   } catch (error) {
     console.error(error);
   }
@@ -110,58 +102,51 @@ router.post("/add-owner", async (req, res) => {
     name: req.body.name,
     email: req.body.email,
     password: req.body.password,
-    date: req.body.date
-  })
+    date: req.body.date,
+  });
   try {
-    const result = await owner.save()
-      .catch(err => {
-        throw err
-      })
-    res.send(result)
+    const result = await owner.save().catch((err) => {
+      throw err;
+    });
+    res.send(result);
   } catch (error) {
     console.error(error);
   }
 });
 
 router.get("/owner/:id", async (req, res) => {
-  const id = req.params.id
+  const id = req.params.id;
   try {
-    const owner = await Owner.findById(id)
-      .catch((err) => {
-        throw err
-      })
-    res.status(200).send(owner)
+    const owner = await Owner.findById(id).catch((err) => {
+      throw err;
+    });
+    res.status(200).send(owner);
   } catch (error) {
     console.log(error);
   }
 });
 
 router.patch("/owner/:id/update", async (req, res) => {
-  const ownerId = req.params.id
-  const {
-    name,
-    email,
-    password
-  } = req.body
+  const ownerId = req.params.id;
+  const { name, email, password } = req.body;
   const updatedOwner = await Owner.findByIdAndUpdate(ownerId, {
     name: name,
     email: email,
-    password: password
-  })
-  if (!updatedOwner) return res.status(400).send('Owner does not updated!')
-  res.status(200).send('Owner updated successfully!')
+    password: password,
+  });
+  if (!updatedOwner) return res.status(400).send("Owner does not updated!");
+  res.status(200).send("Owner updated successfully!");
 });
 
 router.delete("/owner/:id/delete", async (req, res) => {
-  const id = req.params.id
+  const id = req.params.id;
   try {
-    const owner = await Owner.findByIdAndRemove(id)
-      .catch(err => {
-        console.error(err);
-      })
-    res.status(200).json('Your Owner has been removed successfully')
+    const owner = await Owner.findByIdAndRemove(id).catch((err) => {
+      console.error(err);
+    });
+    res.status(200).json("Your Owner has been removed successfully");
   } catch (error) {
-    console.error(error)
+    console.error(error);
   }
 });
 
